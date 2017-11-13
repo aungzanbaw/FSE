0f4eaa0f004551d4ddf7fc3a801d6eb8ba049b3c
aungzanbaw <aungzanbaw@gmail.com>
2017-11-13 08:15:28 +0630

    More RxJS

--- RxJS/1_converting.js
+++ RxJS/1_converting.js
@@ -14,7 +14,7 @@ Rx.Obserable.fromPromise(fetch('/api'))
 let exists = Rx.Obserable.bindCallback(fs.exists)
 exists('foo.txt').subscribe(e => console.log('Does file exist', e))
 
-// Rx is yet another loadash for events
+// Rx is yet another loadash for events, Rx is the underscore.js for events
 
 // Observable: represents the idea of an invokable collection of future values or events.
 // Observer: is a collection of callbacks that knows how to listen to values delivered by the Observable.
@@ -22,3 +22,11 @@ exists('foo.txt').subscribe(e => console.log('Does file exist', e))
 // Operators: are pure functions that enable a functional programming style of dealing with collections with operations like map, filter, concat, flatMap, etc.
 // Subject: is the equivalent to an EventEmitter, and the only way of multicasting a value or event to multiple Observers.
 // Schedulers: are centralized dispatchers to control concurrency, allowing us to coordinate when computation happens on e.g. setTimeout or requestAnimationFrame or others.
+
+[ 14, 9, 5, 2, 10, 13, 4 ].filter( x => x % 2 == 0 )
+[ 14, 2, 10, 4 ]
+
+// Think of it as an asynchronous immutable array.
+// apply map, filter, reduce to both
+// merge, delay, concat, buffer, distinct, first, last, zip, startWith, window, takeUntil, skip, scan, sample, amb, join, flatMap.
+// a common UI paradigm of receiving completion suggestions while the user is typing input.

1a29c843f93b8c14feec5acbdfcc18a2e8acd0e3
aungzanbaw <aungzanbaw@gmail.com>
2018-04-25 17:35:38 +0630

    Configure in package.json for up start script

---
+++ Webpack(exercise)/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component{
+    render(){
+        return(
+            <div> 
+                <h1>Hello React powered by Webpack</h1>   
+            </div>
+        )
+    }
+}
+
+export default App

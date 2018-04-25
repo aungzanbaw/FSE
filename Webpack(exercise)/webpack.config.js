06d52e9ba3e75540c3c17e265933dcd105441b98
aungzanbaw <aungzanbaw@gmail.com>
2018-04-25 17:26:41 +0630

    Add webpack config and babelrc

---
+++ Webpack(exercise)/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path'),
+    HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}

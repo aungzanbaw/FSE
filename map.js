94d81d8e6b88f2f7f63c1c9c958fd4e7eb17b92a
aungzanbaw <aungzanbaw@gmail.com>
2018-04-28 11:36:46 +0630

    Map on objects

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let animals = [
+const animals = [
   {name: 'EEE', type: 'cat'},
   {name: 'FFF', type: 'fish'},
   {name: 'BBB', type: 'rabbit'},
@@ -9,16 +9,28 @@ let animals = [
   {name: 'DDD', type: 'dog'},
   {name: 'AAA', type: 'rabbit'}
 ]
+// combine with arrow fun
+const names = animals.map(a => a.name )
 
-// let names = animals.map(a => a.name )
-//
-// let names = animals.map(function(animal){
-//   return animal.name
-// })
+const names = animals.map(function(animal){
+  return animal.name
+})
 
-// let names = []
-// for (var i = 0; i < animals.length; i++) {
-//   names.push(animals[i].name)
-// }
-//
-// console.log(names) 
+const names = []
+for (var i = 0; i < animals.length; i++) {
+  names.push(animals[i].name)
+}
+
+console.log(names) 
+
+const cars = {
+  toyota: {name: 'Crown', type: 'saloon'},
+  nissan: {name: 'Sunny', type: 'saloon'},
+  honda: {name: 'Civic', type: 'saloon'},  
+  daihatsu: {name: 'Comapgno', type: 'saloon'},
+  suzuki: {name: 'Ciaz', type: 'saloon'},
+  mazda: {name: 'Mazda3', type: 'SUV'},
+  mitsubishi: {name: 'Eclipse', type: 'SUV'}
+}
+
+Object.keys(cars).map( car => console.log(car, cars[car]))
